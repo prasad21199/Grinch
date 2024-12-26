0c7f6df52350cb43dce67e030d6701517a921df4
prasadk <prasadk@LAPTOP-OR2KO8HI>
2024-12-26 12:13:15 +0530

    me

---
+++ PtronN/src/test/java/com/ptron/testscript/CheckJenk.java
@@ -0,0 +1,20 @@
+package com.ptron.testscript;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CheckJenk {
+	@Test
+	public void jenk()
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+		System.out.println(driver.getTitle());
+		driver.quit();
+		Reporter.log("success",true);
+		
+	}
+}
